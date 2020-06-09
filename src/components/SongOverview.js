9aae4eb8578100e6731a3b6c6ee76094bae98db4
NatalieCastien <natalie.castien@gmail.com>
2020-06-09 10:44:58 +0200

    api integration getsongs, addsong, deletesong

--- src/components/SongOverview.js
+++ src/components/SongOverview.js
@@ -7,46 +7,79 @@ class SongOverview extends Component {
   constructor() {
     super();
     this.state = {
-      songs: [
-        { id: 1, title: "thisisme", artist: "me", genre: "rock", rating: 5 },
-        { id: 2, title: "hello", artist: "adele", genre: "jazz", rating: 3 },
-      ],
+      songs: [],
     };
     this.addSong = this.addSong.bind(this);
     this.deleteSong = this.deleteSong.bind(this);
+    this.getSongs = this.getSongs.bind(this);
+  }
+
+  getSongs() {
+    fetch("https://music-database-1dcdd.firebaseio.com/songs.json")
+      .then((response) => response.json())
+      .then((data) => {
+        const result = Object.keys(data).map((key) => ({
+          id: key,
+          title: data[key].title,
+          artist: data[key].artist,
+          genre: data[key].genre,
+          rating: data[key].rating,
+        }));
+        this.setState({ songs: result });
+        console.log(result);
+      });
+    console.log(this.state);
+  }
+
+  componentDidMount() {
+    this.getSongs();
   }
 
   addSong(song) {
     console.log(song);
     console.log("halo");
-    this.setState((prevState) => {
-      const newState = {
-        songs: [
-          ...prevState.songs,
-          {
-            id: 3,
-            title: song.title,
-            artist: song.artist,
-            genre: song.genre,
-            rating: song.rating,
-          },
-        ],
-      };
-      return newState;
-    });
+    const data = {
+      title: song.title,
+      artist: song.artist,
+      genre: song.genre,
+      rating: song.rating,
+    };
+    const requestOptions = {
+      method: "POST",
+      body: JSON.stringify(data),
+    };
+    fetch(
+      "https://music-database-1dcdd.firebaseio.com/songs.json",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+    this.getSongs();
   }
 
-  deleteSong(songToDelete) {
+  deleteSong(id) {
     // console.log("deleted:");
     // console.log(songToDelete);
-    this.setState(() => {
-      const newState = {
-        songs: this.state.songs.filter((song) => {
-          return song.title !== songToDelete.title;
-        }),
-      };
-      return newState;
-    });
+
+    const baseUrl = "https://music-database-1dcdd.firebaseio.com";
+    const apiUrl = `${baseUrl}/songs/${id}.json`;
+    fetch(apiUrl, { method: "DELETE" })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        this.getSongs();
+      });
+
+    // this.setState(() => {
+    //   const newState = {
+    //     songs: this.state.songs.filter((song) => {
+    //       return song.title !== songToDelete.title;
+    //     }),
+    //   };
+    //   return newState;
+    // });
   }
 
   render() {
